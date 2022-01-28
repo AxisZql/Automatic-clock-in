5ca7f82ed96a5a61994e1502832846cfc503af58
Axis <59349697+AxisZql@users.noreply.github.com>
2022-01-29 00:20:44 +0800

    Update settings.py

--- autodk/settings.py
+++ autodk/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-prqmr$wz-+8nbm)p2asrlcymr%cgx#=s@vo4u5u4o!7wokn3_l'
+SECRET_KEY = 'this is the django key for you'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
